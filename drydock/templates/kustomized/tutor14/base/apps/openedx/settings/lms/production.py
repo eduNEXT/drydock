64a7d3a7f2fde9f1d011f733c543c03d4f5d7e9e
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-10-11 18:00:29 -0400

    feat: add templates with tutor14 support

---
+++ drydock/templates/kustomized/tutor14/base/apps/openedx/settings/lms/production.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+from lms.envs.production import *
+
+{% include "apps/openedx/settings/partials/common_lms.py" %}
+
+ALLOWED_HOSTS = [
+    ENV_TOKENS.get("LMS_BASE"),
+    FEATURES["PREVIEW_LMS_BASE"],
+    "lms",
+]
+
+{% if ENABLE_HTTPS %}
+# Properly set the "secure" attribute on session/csrf cookies. This is required in
+# Chrome to support samesite=none cookies.
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "None"
+{% else %}
+# When we cannot provide secure session/csrf cookies, we must disable samesite=none
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SAMESITE = "Lax"
+{% endif %}
+
+# CMS authentication
+IDA_LOGOUT_URI_LIST.append("{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ CMS_HOST }}/logout/")
+
+# Required to display all courses on start page
+SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
+
+{{ patch("openedx-lms-production-settings") }}
---
+++ drydock/templates/kustomized/tutor14/base/apps/openedx/settings/partials/common_lms.py
@@ -0,0 +1,35 @@
+{% include "apps/openedx/settings/partials/common_all.py" %}
+
+######## Common LMS settings
+LOGIN_REDIRECT_WHITELIST = ["{{ CMS_HOST }}"]
+
+# Better layout of honor code/tos links during registration
+REGISTRATION_EXTRA_FIELDS["terms_of_service"] = "required"
+REGISTRATION_EXTRA_FIELDS["honor_code"] = "hidden"
+
+# Fix media files paths
+PROFILE_IMAGE_BACKEND["options"]["location"] = os.path.join(
+    MEDIA_ROOT, "profile-images/"
+)
+
+COURSE_CATALOG_VISIBILITY_PERMISSION = "see_in_catalog"
+COURSE_ABOUT_VISIBILITY_PERMISSION = "see_about_page"
+
+# Allow insecure oauth2 for local interaction with local containers
+OAUTH_ENFORCE_SECURE = False
+
+# Email settings
+DEFAULT_EMAIL_LOGO_URL = LMS_ROOT_URL + "/theming/asset/images/logo.png"
+BULK_EMAIL_SEND_USING_EDX_ACE = True
+
+# Make it possible to hide courses by default from the studio
+SEARCH_SKIP_SHOW_IN_CATALOG_FILTERING = False
+
+# Create folders if necessary
+for folder in [DATA_DIR, LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE, ORA2_FILEUPLOAD_ROOT]:
+    if not os.path.exists(folder):
+        os.makedirs(folder, exist_ok=True)
+
+{{ patch("openedx-lms-common-settings") }}
+
+######## End of common LMS settings
