2f71f2ea89a216fdcf26a58f261173f4d05c5d44
henrrypg <henrry.pulgarin@edunext.co>
2023-10-06 09:22:58 -0500

    feat: add sentry plugin

--- drydock/plugin.py
+++ drydock/plugin.py
@@ -28,9 +28,11 @@ config = {
         "NEWRELIC_LICENSE_KEY": "",
         "CUSTOM_CERTS": {},
         "DEBUG": False,
-        "ENABLE_CELERY_TUNING": False,
-        "ENABLE_MULTITENANCY": False,
-        "ENABLE_SCORM": False,
+        "ENABLE_CELERY_TUNING": True,
+        "ENABLE_MULTITENANCY": True,
+        "ENABLE_SCORM": True,
+        "ENABLE_SENTRY": True,
+        "SENTRY_DSN": "",
     },
     # Add here settings that don't have a reasonable default for all users. For
     # instance: passwords, secret keys, etc.
