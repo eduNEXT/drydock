f43fb1819d719087d13a2b58437b5b15b30222c8
Henrry Pulgarin <39854568+Henrrypg@users.noreply.github.com>
2023-12-19 13:59:24 -0500

    feat: add support to tutor palm (#57)
    BREAKING CHANGE: Drops support to python 3.7

--- setup.py
+++ setup.py
@@ -38,13 +38,9 @@ setup(
     long_description=load_readme(),
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    python_requires=">=3.7",
-    install_requires=["tutor", "packaging"],
-    entry_points={
-        "tutor.plugin.v1": [
-            "drydock = drydock.plugin",
-            "drydock-backups = drydock_backups.plugin",
-        ]
+    python_requires=">=3.8",
+    install_requires=["tutor>=16.0.0,<17.0.0"],
+    entry_points={"tutor.plugin.v1": ["drydock = drydock.plugin"]
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -52,7 +48,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
