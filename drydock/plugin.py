f5c93cfcbe70b8a6cd2622a6cb5bbe26c6265ed8
henrrypg <henrry.pulgarin@edunext.co>
2024-03-07 10:42:44 -0500

    feat: set it as tutor filter, add priority

--- drydock/plugin.py
+++ drydock/plugin.py
@@ -2,41 +2,82 @@ from glob import glob
 import os
 import pkg_resources
 
-from tutor import hooks
+import typing as t
+
+import functools
+from tutor import hooks as tutor_hooks
+from .hooks import SYNC_WAVES_ORDER_ATTRS_TYPE, SYNC_WAVES_ORDER
 
 from .__about__ import __version__
 
-PRIORITY_LIST = [
-    ('job', 'drydock-mysql-job'),
-    ('job', 'drydock-notes-job-mysql'),
-    ('job', 'drydock-mongodb-job'),
-    ('job', 'drydock-minio-job'),
-    ('job', 'drydock-mfe-lms-job'),
-    ('job', 'drydock-lms-job'),
-    ('job', 'drydock-forum-job'),
-    ('job', 'drydock-cms-job'),
 
-    ('deployment', 'lms'),
-    ('deployment', 'cms'),
-    ('deployment', 'lms-worker'),
-    ('deployment', 'cms-worker'),
-    ('deployment', 'forum'),
+CORE_SYNC_WAVES_ORDER: t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE] = {
+    "jobs": {
+        'drydock-mysql-job': 1,
+        'drydock-mongodb-job': 1,
+        'drydock-lms-job': 2,
+        'drydock-cms-job': 3,
+        'drydock-notes-job-mysql': 4,
+        'drydock-minio-job': 4,
+        'drydock-mfe-lms-job': 4,
+        'drydock-forum-job': 4
+    },
+    "deployments": {
+        'lms': 1,
+        'cms': 1,
+        'lms-worker': 1,
+        'cms-worker': 1,
+        'forum': 1,
+        'cms-debug': 1,
+        'lms-debug': 1,
+    },
+    "ingresses": {
+        'ingress-debug': 1,
+    },
+}
 
-    ('deployment', 'cms-debug'),
-    ('deployment', 'lms-debug'),
-    ('ingress', 'ingress-debug'),
+# The core sync-waves configs are added with a high priority, such that other users can override or
+# remove them.
+@SYNC_WAVES_ORDER.add(priority=tutor_hooks.priorities.HIGH)
+def _add_core_sync_waves_order(sync_waves_config: t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]) -> t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]:
+    sync_waves_config.update(CORE_SYNC_WAVES_ORDER)
+    return sync_waves_config
 
-    ('deployment', 'lms-debug'),
-    ('deployment', 'cms-debug'),
-    ('ingress', 'ingress-debug'),
-]
 
-def get_priority(kind, name):
-    """ Return the priority of a k8s object."""
-    for i, (k, n) in enumerate(PRIORITY_LIST):
-        if k == kind and n == name:
-            return i + 1
-    return len(PRIORITY_LIST)
+@functools.lru_cache(maxsize=None)
+def get_sync_waves_order() -> t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]:
+    """
+    This function is cached for performance.
+    """
+    return SYNC_WAVES_ORDER.apply({})
+
+
+@tutor_hooks.Actions.PLUGIN_LOADED.add()
+def _clear_sync_waves_order_cache(_name: str) -> None:
+    """
+    Don't forget to clear cache, or we'll have some strange surprises...
+    """
+    get_sync_waves_order.cache_clear()
+
+
+def iter_sync_waves_order() -> t.Iterable[t.Tuple[str, SYNC_WAVES_ORDER_ATTRS_TYPE]]:
+    """
+    Yield:
+        (name, dict)
+    """
+    yield from get_sync_waves_order().items()
+
+
+def get_sync_waves_for_resource(resource_name: str, kind: str) -> SYNC_WAVES_ORDER_ATTRS_TYPE:
+    """
+    Args:
+        resource_name: the name of the resource
+        kind: the kind of the resource
+
+    Returns:
+        dict
+    """
+    return get_sync_waves_order()[kind][resource_name]
 
 ################# Configuration
 config = {
@@ -73,8 +114,8 @@ config = {
     },
 }
 
-hooks.Filters.CONFIG_DEFAULTS.add_items([("OPENEDX_DEBUG_COOKIE", "ednx_enable_debug")])
-hooks.Filters.CONFIG_OVERRIDES.add_items([
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items([("OPENEDX_DEBUG_COOKIE", "ednx_enable_debug")])
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items([
         # This values are not prefixed with DRYDOCK_
         ("MONGODB_ROOT_USERNAME", ""),
         ("MONGODB_ROOT_PASSWORD", ""),
@@ -85,10 +126,10 @@ hooks.Filters.CONFIG_OVERRIDES.add_items([
 ################# except maybe for educational purposes :)
 
 # Plugin templates
-hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
     pkg_resources.resource_filename("drydock", "templates")
 )
-hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
         ("drydock/build", "plugins"),
         ("drydock/apps", "plugins"),
@@ -96,9 +137,10 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     ],
 )
 
-hooks.Filters.ENV_TEMPLATE_VARIABLES.add_items(
+tutor_hooks.Filters.ENV_TEMPLATE_VARIABLES.add_items(
     [
-        ('get_priority', get_priority),
+        ('iter_sync_waves_order', iter_sync_waves_order),
+        ('get_sync_waves_for_resource', get_sync_waves_for_resource),
     ]
 )
 
@@ -110,19 +152,19 @@ for path in glob(
     )
 ):
     with open(path, encoding="utf-8") as patch_file:
-        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
 # Load all configuration entries
-hooks.Filters.CONFIG_DEFAULTS.add_items(
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
     [
         (f"DRYDOCK_{key}", value)
         for key, value in config["defaults"].items()
     ]
 )
-hooks.Filters.CONFIG_UNIQUE.add_items(
+tutor_hooks.Filters.CONFIG_UNIQUE.add_items(
     [
         (f"DRYDOCK_{key}", value)
         for key, value in config["unique"].items()
     ]
 )
-hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
