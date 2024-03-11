4b9094af7614c9ad19dacb7a818a30728705d35f
henrrypg <henrry.pulgarin@edunext.co>
2024-03-11 11:09:52 -0500

    feat: add priority filter

--- drydock/plugin.py
+++ drydock/plugin.py
@@ -4,10 +4,15 @@ import os
 import click
 import pkg_resources
 
+import typing as t
+
+import functools
+from .hooks import SYNC_WAVES_ORDER_ATTRS_TYPE, SYNC_WAVES_ORDER
+
+from tutor import hooks as tutor_hooks
 from tutor import env as tutor_env
-from tutor import serialize
+from tutor import serialize, types
 from tutor import config as tutor_config
-from tutor import hooks, types
 
 from .__about__ import __version__
 
@@ -39,18 +44,18 @@ def _load_jobs(tutor_conf: types.Config) -> Iterable[Any]:
 
 def get_init_tasks():
     """Return the list of init tasks to run."""
-    init_tasks = list(hooks.Filters.CLI_DO_INIT_TASKS.iterate())
+    init_tasks = list(tutor_hooks.Filters.CLI_DO_INIT_TASKS.iterate())
     context = click.get_current_context().obj
     tutor_conf = tutor_config.load(context.root)
 
     # Standarize deprecated COMMANDS_INIT and COMMANDS_PRE_INIT Filter
     pre_init_tasks = []
-    for service, init_path in hooks.Filters.COMMANDS_PRE_INIT.iterate():
+    for service, init_path in tutor_hooks.Filters.COMMANDS_PRE_INIT.iterate():
         pre_init_tasks.append((service, tutor_env.read_template_file(*init_path)))
 
     init_tasks = pre_init_tasks + init_tasks
 
-    for service, init_path in list(hooks.Filters.COMMANDS_INIT.iterate()):
+    for service, init_path in list(tutor_hooks.Filters.COMMANDS_INIT.iterate()):
         init_tasks.append((service, tutor_env.read_template_file(*init_path)))
 
     upgrade_jobs = _get_upgrade_jobs(tutor_conf)
@@ -65,7 +70,7 @@ def get_init_tasks():
 
             render_command = tutor_env.render_str(tutor_conf, command)
 
-            template['metadata']['name'] = 'drydock-' + template['metadata']['name'] + '-' + str(i)
+            template['metadata']['name'] = 'drydock-' + template['metadata']['name']
             template['metadata']['labels'] = {
                 'drydock.io/component': 'job',
                 'drydock.io/target-service': template['metadata']['name'],
@@ -117,6 +122,53 @@ def get_upgrade_list():
             upgrade_list.append(name.lower())
     return upgrade_list
 
+
+CORE_SYNC_WAVES_ORDER: t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE] = {
+}
+
+# The core sync-waves configs are added with a high priority, such that other users can override or
+# remove them.
+@SYNC_WAVES_ORDER.add(priority=tutor_hooks.priorities.HIGH)
+def _add_core_sync_waves_order(sync_waves_config: t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]) -> t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]:
+    sync_waves_config.update(CORE_SYNC_WAVES_ORDER)
+    return sync_waves_config
+
+
+@functools.lru_cache(maxsize=None)
+def get_sync_waves_order() -> t.Dict[str, SYNC_WAVES_ORDER_ATTRS_TYPE]:
+    """
+    This function is cached for performance.
+    """
+    return SYNC_WAVES_ORDER.apply({})
+
+
+@tutor_hooks.Actions.PLUGIN_LOADED
+def _clear_sync_waves_order_cache(_name: str) -> None:
+    """
+    Don't forget to clear cache, or we'll have some strange surprises...
+    """
+    get_sync_waves_order.cache_clear()
+
+
+def iter_sync_waves_order() -> t.Iterable[t.Tuple[str, SYNC_WAVES_ORDER_ATTRS_TYPE]]:
+    """
+    Yield:
+        (name, dict)
+    """
+    yield from get_sync_waves_order().items()
+
+
+def get_sync_waves_for_resource(resource_name: str, kind: str) -> SYNC_WAVES_ORDER_ATTRS_TYPE:
+    """
+    Args:
+        resource_name: the name of the resource
+        kind: the kind of the resource
+    Returns:
+        dict
+    """
+    return get_sync_waves_order()[kind][resource_name]
+
+
 ################# Configuration
 config = {
     # Add here your new settings
@@ -153,8 +205,8 @@ config = {
     },
 }
 
-hooks.Filters.CONFIG_DEFAULTS.add_items([("OPENEDX_DEBUG_COOKIE", "ednx_enable_debug")])
-hooks.Filters.CONFIG_OVERRIDES.add_items([
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items([("OPENEDX_DEBUG_COOKIE", "ednx_enable_debug")])
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items([
         # This values are not prefixed with DRYDOCK_
         ("MONGODB_ROOT_USERNAME", ""),
         ("MONGODB_ROOT_PASSWORD", ""),
@@ -165,11 +217,11 @@ hooks.Filters.CONFIG_OVERRIDES.add_items([
 ################# except maybe for educational purposes :)
 
 # Plugin templates
-hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
     pkg_resources.resource_filename("drydock", "templates")
 )
 
-hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
         ("drydock/build", "plugins"),
         ("drydock/apps", "plugins"),
@@ -185,27 +237,29 @@ for path in glob(
     )
 ):
     with open(path, encoding="utf-8") as patch_file:
-        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
 # Load all configuration entries
-hooks.Filters.CONFIG_DEFAULTS.add_items(
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
     [
         (f"DRYDOCK_{key}", value)
         for key, value in config["defaults"].items()
     ]
 )
-hooks.Filters.CONFIG_UNIQUE.add_items(
+tutor_hooks.Filters.CONFIG_UNIQUE.add_items(
     [
         (f"DRYDOCK_{key}", value)
         for key, value in config["unique"].items()
     ]
 )
-hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
-hooks.Filters.ENV_TEMPLATE_VARIABLES.add_items(
+tutor_hooks.Filters.ENV_TEMPLATE_VARIABLES.add_items(
     [
         ('get_init_tasks', get_init_tasks),
         ('get_upgrade_list', get_upgrade_list),
+        ('iter_sync_waves_order', iter_sync_waves_order),
+        ('get_sync_waves_for_resource', get_sync_waves_for_resource),
     ]
 )
 
@@ -214,4 +268,4 @@ with open(
     pkg_resources.resource_filename("drydock", "templates/drydock/task/mongodb/init"),
     encoding="utf-8",
 ) as fi:
-    hooks.Filters.CLI_DO_INIT_TASKS.add_item(("mongodb", fi.read()), priority=hooks.priorities.HIGH)
+    tutor_hooks.Filters.CLI_DO_INIT_TASKS.add_item(("mongodb", fi.read()), priority=tutor_hooks.priorities.HIGH)
